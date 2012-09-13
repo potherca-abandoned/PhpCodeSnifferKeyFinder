4afc43204851859f261a2e5329938d62401ec27e
Ben Peachey <potherca@gmail.com>
2012-09-13 14:57:37 +0200

    Adds first simple unittests for CodeSnifferKeyFinder::findKeysInFolder()

--- CodeSnifferKeyFinder/test.CodeSnifferKeyFinder.php
+++ CodeSnifferKeyFinder/test.CodeSnifferKeyFinder.php
@@ -9,36 +9,75 @@ class CodeSnifferKeyFinderTest extends PHPUnit_Framework_TestCase
      */
     protected $object;
 
-	public static function setUpBeforeClass()
-	{
-		include_once 'class.CodeSnifferKeyFinder.php';
-	}
+    public static function setUpBeforeClass()
+    {
+        include_once 'class.CodeSnifferKeyFinder.php';
+    }
 
-	/**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
     protected function setUp()
     {
         $this->object = new CodeSnifferKeyFinder;
     }
 
     /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
+     * @test
+     * @covers            CodeSnifferKeyFinder::findKeysInFolder
+     *
+     * @expectedException PHPUnit_Framework_Error
+     *
+     * @dataProvider provideInvalidParameter_For_FindKeysInFolder
+     *
+     * @param $p_mSubject
      */
-    protected function tearDown()
+    public function findKeysInFolder_TriggersError_WhenCalledWithSomethingOtherThanRecursiveDirectoryIterator($p_mSubject)
     {
+        $this->object->findKeysInFolder($p_mSubject);
     }
 
     /**
-	 * @test
-     * @covers CodeSnifferKeyFinder::findKeysInFolder
+     * @test
+     * @covers   CodeSnifferKeyFinder::findKeysInFolder
+     *
+     * @@dataProvider provideValidParameter_For_FindKeysInFolder
+     *
+     * @param $p_oDirectoryIterator
      */
-    public function findKeysInFolder()
+    public function findKeysInFolder_ExpectsRecursiveDirectoryIterator_WhenCalled($p_oDirectoryIterator)
     {
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
+        $this->object->findKeysInFolder($p_oDirectoryIterator);
+    }
+
+    /**
+     * @test
+     * @covers   CodeSnifferKeyFinder::findKeysInFolder
+     *
+     * @@dataProvider provideValidParameter_For_FindKeysInFolder
+     *
+     * @param $p_oDirectoryIterator
+     */
+    public function findKeysInFolder_returnsArray_WhenCalledRecursiveDirectoryIterator($p_oDirectoryIterator)
+    {
+        $this->object->findKeysInFolder($p_oDirectoryIterator);
+    }
+
+///////////////////////////////// DataProviders \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    public function provideInvalidParameter_For_FindKeysInFolder()
+    {
+        return array(
+              array(null)
+            , array(true)
+            , array(false)
+            , array(0)
+            , array(2)
+            , array('foo')
+        );
+    }
+
+    public function provideValidParameter_For_FindKeysInFolder()
+    {
+        $oDirectoryIterator = new RecursiveDirectoryIterator(__DIR__);
+        return array(
+            array($oDirectoryIterator)
         );
     }
 }
